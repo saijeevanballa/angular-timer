cca2decc1f640c7e154bb8f5e333a90132f6211d
saijeevanballa <saijeevanballa@gmail.com>
2020-04-10 08:21:39 +0530

    2 files modified

--- src/app/components/timer/timer.component.ts
+++ src/app/components/timer/timer.component.ts
@@ -15,27 +15,17 @@ export class TimerComponent implements OnInit {
     setInterval(() => {
       this.time += 1;
       // console.log(this.time)
-    }, 100);
+    }, 1);
   }
 
-  sec2time(timeInSeconds = this.time) {
-    var pad = function(num, size) {
-        return ("000" + num).slice(size * -1);
-      },
-      time: any = parseFloat(timeInSeconds).toFixed(3),
-      hours = Math.floor(time / 60 / 60),
-      minutes = Math.floor(time / 60) % 60,
-      seconds = Math.floor(time - minutes * 60),
-      milliseconds = time.slice(-3);
-    console.log(this.time);
-    return (
-      pad(hours, 2) +
-      " : " +
-      pad(minutes, 2) +
-      " : " +
-      pad(seconds, 2) +
-      " : " +
-      pad(milliseconds, 3)
-    );
+  ms2time(timeInMs = this.time) {
+    var pad = (num, size) => ("000" + num).slice(size * -1),
+      time: any = parseFloat(timeInMs),
+      hours = Math.floor(time / 3600000),
+      minutes = Math.floor(time / 60000) % 60,
+      seconds = Math.floor(time / 1000) % 60 ,
+      milliseconds = time.length >= 3 ? time.slice(-2) : time;
+    // console.log(this.time);
+    return `${pad(hours, 2)} : ${pad(minutes, 2)} : ${pad(seconds, 2)} : ${pad(milliseconds,2)}`;
   }
 }
